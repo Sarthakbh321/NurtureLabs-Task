153f1490d3666b1ae97fc801a0d961c9a5917d5d
Sarthak <sarthakbh321@gmail.com>
2020-10-27 23:30:33 +0530

    rating filter done

--- src/components/FilterBar/FilterBar.js
+++ src/components/FilterBar/FilterBar.js
@@ -1,10 +1,28 @@
 import React, { useState } from "react";
 import "./FilterBar.css";
 import FilterInput from "../FilterInput/FilterInput";
+import { Checkbox, Grid, withStyles } from "@material-ui/core";
+import { Star } from "@material-ui/icons";
+import { green } from "@material-ui/core/colors";
 
 function FilterBar() {
 	const [expertFilters, setExpertFilters] = useState(["Strategy", "Hello"]);
 	const [focusFilters, setFocusFilters] = useState(["Focus Area"]);
+
+	const [checked, setChecked] = useState([false, false, false, false]);
+
+	const handleCheckbox = (i) => {
+		let arr = checked;
+		arr[i] = !arr[i];
+		setChecked(arr);
+		console.log(arr);
+	};
+
+	const renderStars = (i) => {
+		return [...Array(i)].map((star, i) => (
+			<Star color="secondary" fontSize="small" />
+		));
+	};
 
 	return (
 		<div className="filter-bar">
@@ -26,6 +44,24 @@ function FilterBar() {
 				</div>
 				<div className="m5-bottom">
 					<h5 className="light-gray">RATINGS</h5>
+					<Grid container spacing={0}>
+						<Grid item xs={12} sm={6} className="checkbox-row">
+							<RatingCheck onChange={() => handleCheckbox(3)} />
+							{renderStars(5)}
+						</Grid>
+						<Grid item xs={12} sm={6} className="checkbox-row">
+							<RatingCheck onChange={() => handleCheckbox(2)} />
+							{renderStars(4)}
+						</Grid>
+						<Grid item xs={12} sm={6} className="checkbox-row">
+							<RatingCheck onChange={() => handleCheckbox(1)} />
+							{renderStars(3)}
+						</Grid>
+						<Grid item xs={12} sm={6} className="checkbox-row">
+							<RatingCheck onChange={() => handleCheckbox(0)} />
+							{renderStars(2)}
+						</Grid>
+					</Grid>
 				</div>
 			</div>
 		</div>
@@ -33,3 +69,12 @@ function FilterBar() {
 }
 
 export default FilterBar;
+
+const RatingCheck = withStyles({
+	root: {
+		"&$checked": {
+			color: green[600],
+		},
+	},
+	checked: {},
+})((props) => <Checkbox color="default" {...props} />);
--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const theme = createMuiTheme({
 			main: "#f18354",
 		},
 	},
+
 	typography: {
 		fontFamily:
 			"-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif",
